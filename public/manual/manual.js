67e2208a0292129628c652d56d6e495ef3693400
anandmstr <anandmstr@github.com>
2017-09-28 19:04:35 +0800

    Added User Manual

---
+++ public/manual/manual.js
@@ -0,0 +1,11 @@
+/*global angular*/
+var app = angular.module('manual', ['ngRoute', 'firebase']);
+
+app.config(['$routeProvider', function ($routeProvider) {
+    'use strict';
+    $routeProvider.when('/manual', {
+        templateUrl: 'manual/manual.html',
+        controller: 'manualCtrl'
+    });
+}]);
+
