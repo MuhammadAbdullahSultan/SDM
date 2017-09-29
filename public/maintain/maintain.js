a8faa4a0f86e327a65853666ce1a7f317172c8c3
MuhammadAbdullahSultan <abdullahsultan14@gmail.com>
2017-09-30 00:52:14 +0800

    Dropdown selection for equipment

--- public/maintain/maintain.js
+++ public/maintain/maintain.js
@@ -67,14 +67,14 @@ app.controller('maintainCtrl', ['$scope', '$firebaseObject', '$firebaseArray', '
         this.checked ? $('.checkboxAO').prop('checked', true) : $('.checkboxAO').prop('checked', false);
     });
  //maximum character in description   
-    $('textarea').keypress(function(){
-
-    if(this.value.length == 50){
-        return false;
-    } else {
-        $("#remainingC").html("Remaining characters : " + (49 - this.value.length));
-    }
-});
+//    $('textarea').keypress(function(){
+//
+//    if(this.value.length == 50){
+//        return false;
+//    } else {
+//        $("#remainingC").html("Remaining characters : " + (49 - this.value.length));
+//    }
+//});
     
      $('.dropdown-menu input').click(function (e) {
      e.stopPropagation();
@@ -148,79 +148,58 @@ $('.dropdown-toggle b').remove().appendTo($('.dropdown-toggle').text($(this).tex
     
     // Counting characters in text area.
     
-    $("#textarea").keyup(function(){
-  $("#count").text("Characters left: " + (100 - $(this).val().length));
+    $("textarea").keyup(function(){
+    $("#remainingC").text("Characters left: " + (50 - $(this).val().length));
 });
+    
+    // SORTING ALGORITHM
+    
+    $(function() {
+		$("table").tablesorter({debug: true})
+		$("a.append").click(appendData);
+		
+		
+	});
+	
+	var lastStudent = 23;
+	var limit = 999999999;
+	
+	function appendData() {
+		
+		var tdTagStart = '<td>';
+		var tdTagEnd = '</td>';
+		var sex = ['male','female'];
+		var major = ['Mathematics','Languages'];
+		
+		
+		for(var i = 0; i < limit; i++) { 
+			var rnd = i % 2;
+			var row = '<tr>';	
+			row += tdTagStart + 'student' + (lastStudent++) + tdTagEnd;
+			row += tdTagStart + major[rnd] + tdTagEnd;
+			row += tdTagStart + sex[rnd] + tdTagEnd;
+			
+			row += tdTagStart +  randomNumber() + tdTagEnd;
+			row += tdTagStart +  randomNumber() + tdTagEnd;
+			row += tdTagStart +  randomNumber() + tdTagEnd;
+			row += tdTagStart +  randomNumber() + tdTagEnd;
+			
+			row += '</tr>';
+			
+			$("table/tbody:first").append(row);
+			
+		};
+		
+		
+		$("table").trigger('update');
+		return false;
+	}
+	
+	function randomNumber() {
+		return Math.floor(Math.random()*101)
+	}
     
 }]);
 
-    $(function () {
-    $(".custom-close").on('click', function() {
-        $('#myModal').modal('hide');
-        });
-    });
-
-    
-    $('body').removeClass('modal-open');
-    $('.modal-backdrop fade in').remove();
-function edit_row(no)
-{
- document.getElementById("edit_button"+no).style.display="none";
- document.getElementById("save_button"+no).style.display="block";
-	
- var name=document.getElementById("name_row"+no);
- var country=document.getElementById("country_row"+no);
- var age=document.getElementById("age_row"+no);
- var group=document.getElementById("group_row"+no);
-	
- var name_data=name.innerHTML;
- var country_data=country.innerHTML;
- var age_data=age.innerHTML;
- var group_data=group.innerHTML;
-	
- name.innerHTML="<input type='text' id='name_text"+no+"' value='"+name_data+"' data-ng-model='editEquipment'>";
- country.innerHTML="<input type='text' id='country_text"+no+"' value='"+country_data+"' data-ng-model='editSystem'>";
- age.innerHTML="<input type='text' id='age_text"+no+"' value='"+age_data+"' data-ng-model='editDescription'>";
- group.innerHTML="<input type='text' id='group_text"+no+"'value='"+group_data+"' data-ng-model='editGroup'>";
-}
-
-function save_row(no)
-{
- var name_val=document.getElementById("name_text"+no).value;
- var country_val=document.getElementById("country_text"+no).value;
- var age_val=document.getElementById("age_text"+no).value;
- var group_val=document.getElementById("group_text"+no).value;
-
- document.getElementById("name_row"+no).innerHTML=name_val;
- document.getElementById("country_row"+no).innerHTML=country_val;
- document.getElementById("age_row"+no).innerHTML=age_val;
- document.getElementById("group_row"+no).innerHTML=group_val;
-
- document.getElementById("edit_button"+no).style.display="block";
- document.getElementById("save_button"+no).style.display="none";
-}
-
-function delete_row(no)
-{
- document.getElementById("row"+no+"").outerHTML="";
-}
-
-function add_row()
-{
- var new_name=document.getElementById("new_name").value;
- var new_country=document.getElementById("new_country").value;
- var new_age=document.getElementById("new_age").value;
- var new_group=document.getElementById("new_group").value;
-	
- var table=document.getElementById("data_table");
- var table_len=(table.rows.length)-1;
- var row = table.insertRow(table_len).outerHTML="<tr id='row"+table_len+"'><td id='name_row"+table_len+"'>"+new_name+"</td><td id='country_row"+table_len+"'>"+new_country+"</td><td id='age_row"+table_len+"'>"+new_age+"</td><td id='group_row"+table_len+"'>"+new_group+"</td><td><input type='button' id='edit_button"+table_len+"' value='Edit' class='edit' onclick='edit_row("+table_len+")'> <input type='button' id='save_button"+table_len+"' value='Save' class='save' onclick='save_row("+table_len+")'> <input type='button' value='Delete' class='delete' onclick='delete_row("+table_len+")'></td></tr>";
-
- document.getElementById("new_name").value="";
- document.getElementById("new_country").value="";
- document.getElementById("new_age").value="";
- document.getElementById("new_group").value="";
-}
-
-$("#myModal .close").click()
+$("#myModal .close").click();
 
